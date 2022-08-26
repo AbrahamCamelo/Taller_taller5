c462e1b2efd0b590ffddc9f58422853a814b1b2f
Abraham Camelo-Guerrero <acamelo@unal.edu.co>
2022-08-25 20:27:30 -0500

    taller 2

--- Taller1/Src/main.c
+++ Taller1/Src/main.c
@@ -7,14 +7,31 @@
  */
 
 #include <stdint.h>
+//Esta es una de las funciones de la libreria <stdint>
+uint8_t variable;
+uint8_t m; int32_t n;
+
+
+char miPriemrString[6] = "Li nux";
+char miPrimerCaracter = 'A';
+
+
 
 unsigned int anos = 0;
 signed short dias = 0;
 
 int main(void)
 {
-	anos = 10;
-	dias = 365;
+	//como saber el tamano de una variable
+	char SizeChar = sizeof(variable);
+
+
+	//operadores
+	m = m + n;
+	m += n;
+
+	m <<= n;
+
 
     /* Loop forever */
 	while(1){
